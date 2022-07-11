2074c3a74ea7f49aa7a291e321ca4000445f17e3
Andrey I <iaa@bork.ru>
2022-07-11 13:41:11 +0200

    Initial Commit

---
+++ BeerApp/BeerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerAppApp.swift
+//  BeerApp
+//
+//  Created by atme on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BeerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
